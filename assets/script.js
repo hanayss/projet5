11e11c1d5a22e223d6a1ca4f21fc53b97836be4b
Hanayss <hanayssdev@gmail.com>
2023-09-21 14:15:45 +0200

    Etape 3

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,21 @@ const slides = [
         tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
     },
 ];
+
+const slideNumber = slides.length;
+
+for (i = 0; i < slideNumber; i++) {
+    console.log(i);
+    const newDotElement = document.createElement("div");
+    newDotElement.classList.add("dot");
+    if (i === 0) {
+        newDotElement.classList.add("dot_selected");
+    }
+    const dotsContainer = document.querySelector(".dots");
+    dotsContainer.appendChild(newDotElement);
+    console.log("newDotElement =>", newDotElement);
+}
+
 const arrowLeftElement = document.querySelector(".arrow_left");
 arrowLeftElement.addEventListener("click", function () {
     console.log("la fleche gauche à été cliquée");
